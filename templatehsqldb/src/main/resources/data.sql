93f4f0122d7e5a1cb96fa6fd1217ae6490592dc9
oopsmails <oopsmails@gmail.com>
2018-01-24 23:00:33 -0500

    move templatehsqldb to bootparent

--- templatehsqldb/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into users(userId, userName, userEmail, address) values (1000, 'Dinesh', 'dinesh@gmail.com', 'Delhi');
-insert into users(userId, userName, userEmail, address) values (1001, 'Kumar', 'kumar@gmail.com', 'Greater Noida');
-insert into users(userId, userName, userEmail, address) values (1002, 'Rajput', 'rajput@gmail.com', 'Noida');
-
-insert into carts(userId, cartType) values (1000, 'ONLINE');
-insert into carts(userId, cartType) values (1000, 'STORE');
-insert into carts(userId, cartType) values (1001, 'ONLINE');
